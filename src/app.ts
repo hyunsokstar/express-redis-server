ee6f9551f78ebb76896185ffec9e155e1820115c
hyunsokstar <hyunsokstar@gmail.com>
2024-03-12 17:14:46 +0900

    auto deploy pm2 21

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ export const createApp = (client: RedisClient) => {
 
 
     app.get("/", (request, response) => {
-        response.status(200).send("auto deploy ! go to ec2 20! ");
+        response.status(200).send("auto deploy ! go to ec2 21! ");
     });
 
     return app;
