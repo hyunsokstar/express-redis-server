a88cc3bcf59b1cb2e5a15ff4762ffc4c33236227
hyunsokstar <hyunsokstar@gmail.com>
2024-03-12 15:35:53 +0900

    update for go to ec2 7

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ export const createApp = (client: RedisClient) => {
 
 
     app.get("/", (request, response) => {
-        response.status(200).send("auto deploy ! go to ec2 6! ");
+        response.status(200).send("auto deploy ! go to ec2 7! ");
     });
 
     return app;
